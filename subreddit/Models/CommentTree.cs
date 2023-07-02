87209c0b9cd107c6693f14372a45eead52f4e2cf
Varunda <trvarunda@gmail.com>
2023-07-01 22:36:12 -0700

    remove https redirect

--- subreddit/Models/CommentTree.cs
+++ subreddit/Models/CommentTree.cs
@@ -6,6 +6,10 @@
 
         public List<CommentTree> Children { get; set; } = new();
 
+        /// <summary>
+        ///     Get a node within a tree
+        /// </summary>
+        /// <param name="commentID">ID of the comment to get</param>
         public CommentTree? GetChild(string commentID) {
             foreach (CommentTree tree in Children) {
                 if (tree.Root.Comment.ID == commentID) {
@@ -21,6 +25,10 @@
             return null;
         }
 
+        /// <summary>
+        ///     Get the roots of each node within a tree using a depth first iteration
+        /// </summary>
+        /// <returns></returns>
         public List<ViewComment> GetList() {
             List<ViewComment> tree = new();
             tree.Add(Root);
